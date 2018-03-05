3d878987c2f4e774d7fb0484efdb550306736d21
senickel <senickel@yahoo.de>
2018-03-05 14:48:45 +0100

    description added

--- R/text_clean.ending.R
+++ R/text_clean.ending.R
@@ -1,14 +1,13 @@
 #' text_clean.ending
 #'
-#' check nominal if they are nominal
-#' @param Click here and there
-#' @keywords nominal
+#'
+#' @param strng It's a string
+
 #' @keywords
 #' @export
-#' @examples 
-#' checking.nominal()
+#' @examples
 #' @importFrom magrittr %>%
-#' 
+#'
 
 text_clean.ending<-function(strng) {
   if (substr(strng,nchar(strng),nchar(strng))%in%c(":",";","/","\\")) substr(strng,1,nchar(strng)-1)
