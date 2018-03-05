5375962b70404554748ee57e92bab8db97b4b587
senickel <senickel@yahoo.de>
2018-03-05 14:42:31 +0100

    import fread from data.table header=TRUE

--- R/read_csv_timestamp.R
+++ R/read_csv_timestamp.R
@@ -6,10 +6,10 @@
 #' @keywords
 #' @export
 #' @examples
-#' get.1.sub(data,split,cols,whichcol,replace)
+#' @importFrom data.table fread
 #'
 read_csv_timestamp<-function(filepath,filename) {
   library(data.table)
 files<-list.files(filepath,pattern=glob2rx(paste0("*",filename,"*csv")),full.names = TRUE) %>% sort
-return(fread(files[length(files)]))
+return(fread(files[length(files)],header=TRUE))
 }
