6bcaa5c99b1f3831afd0032536c45f08974f6c60
senickel <senickel@yahoo.de>
2018-03-05 14:41:39 +0100

    import fwrite from data.table

--- R/write_csv_timestamp.R
+++ R/write_csv_timestamp.R
@@ -1,12 +1,12 @@
 #' write_csv_timestamp function
-#' 
+#'
 #' check ordinal if they are ordinal
 #' @param Click here and there
 #' @keywords ordinal
 #' @keywords
 #' @export
-#' @examples 
-#' get.1.sub(data,split,cols,whichcol,replace)
+#' @examples
+#' @importFrom data.table fwrite
 
 write_csv_timestamp<-function(data,filepath,filename) {
   library(dplyr)
