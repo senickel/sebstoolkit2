353e6fb8102ab1451cfcda98015d7f3e577d2a8f
senickel <senickel@yahoo.de>
2018-03-05 14:43:23 +0100

    deleted library(data.table)

--- R/read_csv_timestamp.R
+++ R/read_csv_timestamp.R
@@ -1,15 +1,13 @@
 #' read_csv_timestamp function
 #'
-#' check ordinal if they are ordinal
-#' @param Click here and there
-#' @keywords ordinal
-#' @keywords
+#'
+#' @param filepath path to file
+#' @param  filename name of the file
 #' @export
 #' @examples
 #' @importFrom data.table fread
 #'
 read_csv_timestamp<-function(filepath,filename) {
-  library(data.table)
 files<-list.files(filepath,pattern=glob2rx(paste0("*",filename,"*csv")),full.names = TRUE) %>% sort
 return(fread(files[length(files)],header=TRUE))
 }
