c495d02c436cad8ef46aedd818ad169b6f49a5c0
senickel <senickel@yahoo.de>
2018-03-12 14:36:15 +0100

    text_clean.whitespace had problems with NA. Fixed.

--- R/text_clean.whitespace.R
+++ R/text_clean.whitespace.R
@@ -1,17 +1,17 @@
 #' text_clean.whitespace
 #'
-#' check nominal if they are nominal
-#' @param Click here and there
-#' @keywords nominal
-#' @keywords
-#' @export
-#' @examples 
-#' checking.nominal()
+#' Cleans double whitespaces and whitespace at the beginning or end of string.
+#' @param vec character string or vector of character strings
+#' @examples
+#' test<-c("te    st"," test ")
+#' test
+#' text_clean.whitespace(test)
 #' @importFrom magrittr %>%
-#' 
+#'
 
 text_clean.whitespace<-function(vec) {
   rt<-lapply(vec,function(strng) {
+    if (is.na(strng)) return(strng)
     while(gregexpr("  ",strng)[[1]][1]!=-1) strng <- strng %>% gsub("  "," ",.)
     if (substr(strng,1,1)==" ") strng<-substr(strng,2,nchar(strng))
     if (substr(strng,nchar(strng),nchar(strng))==" ") strng<-substr(strng,1,nchar(strng)-1)
@@ -19,3 +19,5 @@ text_clean.whitespace<-function(vec) {
   }) %>% unlist
   return(rt)
 }
+
+
