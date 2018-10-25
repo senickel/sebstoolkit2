9ee5edca45a7ae7c3fbd6a394ce8d1b2daa9dc3c
senickel <senickel@yahoo.de>
2018-10-25 12:45:34 +0200

    allows different layers, versatile

--- R/readKML.R
+++ R/readKML.R
@@ -17,38 +17,68 @@
 #' @importFrom rgdal readOGR
 
 
-readKML<-function(file,keep_name_description=FALSE,...) {
+
+readKML <- function(file,keep_name_description=FALSE,layer,...) {
   # Set keep_name_description = TRUE to keep "Name" and "Description" columns
   #   in the resulting SpatialPolygonsDataFrame. Only works when there is
   #   ExtendedData in the kml file.
 
-  sp_obj<-readOGR(file,...)
+  sp_obj<-readOGR(file,layer,...)
   xml1<-read_xml(file)
+  if (!missing(layer)) {
+    different_layers <- xml_find_all(xml1, ".//d1:Folder")
+    layer_names <- different_layers %>%
+      xml_find_first(".//d1:name") %>%
+      xml_contents() %>%
+      xml_text()
+
+    selected_layer <- layer_names==layer
+    if (!any(selected_layer)) stop("Layer does not exist.")
+    xml2 <- different_layers[selected_layer]
+  } else {
+    xml2 <- xml1
+  }
 
   # extract name and type of variables
-  variable_names<-xml_find_first(xml1,".//d1:Schema[@name]") %>%
-    xml2::xml_contents() %>%
-    xml2::xml_attrs() %>%
-    do.call(rbind,.)
+
+  variable_names1 <-
+    xml_find_first(xml2, ".//d1:ExtendedData") %>%
+    xml_children()
+
+  while(variable_names1 %>%
+        xml_attr("name") %>%
+        is.na() %>%
+        any()&variable_names1 %>%
+        xml_children() %>%
+        length>0) variable_names1 <- variable_names1 %>%
+    xml_children()
+
+  variable_names <- variable_names1 %>%
+    xml_attr("name") %>%
+    unique()
 
   # return sp_obj if no ExtendedData is present
   if (is.null(variable_names)) return(sp_obj)
 
-  data<-lapply(seq(nrow(variable_names)),function(x) {
-    x1<-xml_find_all(xml1,paste0(".//d1:SimpleData[@name=\"",variable_names[x,1],"\"]")) %>%
-      xml_contents()
-    if (variable_names[x,2]=="string") return(xml_text(x1))
-    xml_double(x1)
-  }) %>%
-    do.call(cbind,.) %>%
+  data1 <- xml_find_all(xml2, ".//d1:ExtendedData") %>%
+    xml_children()
+
+  while(data1 %>%
+        xml_children() %>%
+        length>0) data1 <- data1 %>%
+    xml_children()
+
+  data <- data1 %>%
+    xml_text() %>%
+    matrix(.,ncol=length(variable_names),byrow = TRUE) %>%
     as.data.frame()
 
-  colnames(data)<-variable_names[,1]
+  colnames(data) <- variable_names
 
   if (keep_name_description) {
-    sp_obj@data<-data
+    sp_obj@data <- data
   } else {
-    try(sp_obj@data<-cbind(sp_obj@data,data),silent=TRUE)
+    try(sp_obj@data <- cbind(sp_obj@data,data),silent=TRUE)
   }
   sp_obj
 }
